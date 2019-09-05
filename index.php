bcdc4bb5bb4c53952bbcfb9c44a105f3b5717717
vanvinh1234 <vanvinhbk90hoangkim@gmail.com>
2019-09-05 15:13:01 +0700

    creat layout web

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <link rel ="stylesheet" type ="text/css" href="style.css">
+</head>
+<body>
+<div class="container">
+    <header><?php include "header.php" ?></header>
+    <footer><?php include "footer.php" ?></footer>
+    <nav><?php include "nav.php" ?></nav>
+    <article><?php include "content.php" ?></article>
+</div>
+</body>
+</html>
